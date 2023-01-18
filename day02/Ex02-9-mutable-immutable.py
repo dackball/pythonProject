cb4f7b3d756511a2a55f5ac5f1e1325e53bbfa41
minguri <122717574+rn1nha@users.noreply.github.com>
2023-01-18 19:51:40 +0900

    ..

--- day02/Ex02-9-mutable-immutable.py
+++ day02/Ex02-9-mutable-immutable.py
@@ -1,5 +1,5 @@
 '''
-mutable - 메모리 값을 변경 가능 객체
+mutable - 메모리 값을 변경 가능 객체  / 주소는 그대로
           리스트(list), 세트(set), 딕셔너리(dict)
 
 '''
@@ -11,7 +11,7 @@ me.append(4)
 print(id(me)) #2505280737672
 
 '''
-immutable = 메모리 값 변경 불가
+immutable = 메모리 값 변경 불가 / 주소가 바뀜 새로 연결
      정수(int), 실수(float), 문자열(str). 튜플(tuple)
 '''
 me = 10
